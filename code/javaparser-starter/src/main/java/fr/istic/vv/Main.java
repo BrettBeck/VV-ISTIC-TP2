bd9e261c4cd683bec1b1f33778ffb0b0a81a67d5
Becker Brett <brett.becker@etudiant.univ-rennes1.fr>
2023-03-10 08:01:18 +0100

    fin Visiteur 1 fichier

--- code/javaparser-starter/src/main/java/fr/istic/vv/Main.java
+++ code/javaparser-starter/src/main/java/fr/istic/vv/Main.java
@@ -1,17 +1,9 @@
 package fr.istic.vv;
 
-import com.github.javaparser.Problem;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.visitor.VoidVisitor;
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.utils.SourceRoot;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main {
 
@@ -33,7 +25,6 @@ public class Main {
             result.ifSuccessful(unit -> unit.accept(printer, null));
             return SourceRoot.Callback.Result.DONT_SAVE;
         });
+        printer.verifyGetters();
     }
-
-
 }
--- code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
+++ code/javaparser-starter/src/main/java/fr/istic/vv/PublicElementsPrinter.java
@@ -4,26 +4,36 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.visitor.VoidVisitorWithDefaults;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 // This class visits a compilation unit and
 // prints all public enum, classes or interfaces along with their public methods
 public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
 
+    List<String> attributes = new ArrayList<>();
+    List<String> getters = new ArrayList<>();
+
     @Override
     public void visit(CompilationUnit unit, Void arg) {
-        for(TypeDeclaration<?> type : unit.getTypes()) {
+        for (TypeDeclaration<?> type : unit.getTypes()) {
             type.accept(this, null);
         }
     }
 
     public void visitTypeDeclaration(TypeDeclaration<?> declaration, Void arg) {
-        if(!declaration.isPublic()) return;
+
+        if (!declaration.isPublic()) return;
         System.out.println(declaration.getFullyQualifiedName().orElse("[Anonymous]"));
-        for(MethodDeclaration method : declaration.getMethods()) {
+        for (MethodDeclaration method : declaration.getMethods()) {
             method.accept(this, arg);
         }
+        for(FieldDeclaration field : declaration.getFields()) {
+            field.accept(this, arg);
+        }
         // Printing nested types in the top level
-        for(BodyDeclaration<?> member : declaration.getMembers()) {
+        for (BodyDeclaration<?> member : declaration.getMembers()) {
             if (member instanceof TypeDeclaration)
                 member.accept(this, arg);
         }
@@ -41,8 +51,23 @@ public class PublicElementsPrinter extends VoidVisitorWithDefaults<Void> {
 
     @Override
     public void visit(MethodDeclaration declaration, Void arg) {
-        if(!declaration.isPublic()) return;
+        if (!declaration.isPublic()) return;
+        if (declaration.getName().toString().contains("get")){
+            String nameGetter = declaration.getName().toString().toLowerCase().replaceAll("get", "");
+            this.getters.add(nameGetter);
+        }
+
+
         System.out.println("  " + declaration.getDeclarationAsString(true, true));
     }
+    @Override
+    public void visit(FieldDeclaration declaration, Void arg) {
+        String fieldName = declaration.getVariables().get(0).toString();
+        this.attributes.add(fieldName);
+    }
 
+    public void verifyGetters() {
+        attributes.removeAll(getters);
+        System.out.println("attribues without getters : " + attributes);
+    }
 }
